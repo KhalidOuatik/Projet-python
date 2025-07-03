363b9833fb0d97a2e45f6b44627fc28ae0915b82
Khalid OUATIK <khalidouatik@MacBook-Air-de-Khalid.local>
2025-07-03 12:23:09 +0200

    Supprime le secret Azure du fichier d'environnement

--- infra/azure_env.sh
+++ infra/azure_env.sh
@@ -1,5 +1,5 @@
 # Variables d'environnement Azure pour Terraform
 export ARM_CLIENT_ID="1c8ed5e4-3220-4844-8b7f-b49743f2bd1c"
-export ARM_CLIENT_SECRET="***REMOVED***"
+# export ARM_CLIENT_SECRET=""
 export ARM_SUBSCRIPTION_ID="3552296d-f3a0-46fc-9b9a-ff3d7910690f"
 export ARM_TENANT_ID="b7b023b8-7c32-4c02-92a6-c8cdaa1d189c"
