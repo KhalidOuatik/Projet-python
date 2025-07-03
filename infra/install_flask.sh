ca343b81e1a59421810617ebe5e308f4f0273717
Khalid OUATIK <khalidouatik@MacBook-Air-de-Khalid.local>
2025-07-03 13:35:55 +0200

    test6

--- infra/install_flask.sh
+++ infra/install_flask.sh
@@ -5,9 +5,9 @@ set -e
 sudo apt-get update
 sudo apt-get install -y python3 python3-pip git
 
-# Clone le repo (remplace par ton repo si besoin)
-git clone https://github.com/<TON-UTILISATEUR>/<TON-REPO>.git app
-cd app
+# Supposé être lancé depuis le dossier où release.zip a été copié
+cd /tmp
+unzip -o release.zip
 pip3 install -r requirements.txt
 
 # Lancer l'app Flask (adapter si besoin)
